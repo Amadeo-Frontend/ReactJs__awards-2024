b0334baa8fa9589b82db9200f0d176d591b95de0
Amadeo Bon <amadeosph@outlook.com>
2024-11-24 02:20:37 -0300

    Initialize repository and setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-400 font-circular-web">
+        Welcome to awards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
