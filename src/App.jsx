3eb4205849f142d6656fe95367987ecd4529a14e
Amadeo Bon <amadeosph@outlook.com>
2024-12-09 11:00:52 -0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative w-screen min-h-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
       <About />
     </main>
